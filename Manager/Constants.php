de7b4670c41ed7cf716200324d5eeacd119e0a90
Rikysamuel <rikysamuel@xtremax.com>
2017-10-24 10:57:55 +0700

    server config

--- Manager/Constants.php
+++ Manager/Constants.php
@@ -1,15 +1,13 @@
 <?php
-	$server = "host = 127.0.0.1 port = 5432";
-	$database = "dbname = absensi";
-	$username = "user = postgres";
-	$password = "password = admin";
+	// $server = "host = 127.0.0.1 port = 5432";
+	// $database = "dbname = absensi";
+	// $username = "user = postgres";
+	// $password = "password = admin";
 
-	// $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-
-	// $server = $url["host"];
-	// $database = substr($url["path"], 1);
-	// $username = $url["user"];
-	// $password = $url["pass"];
+	$server = "host = ec2-54-197-253-210.compute-1.amazonaws.com port = 5432";
+	$database = "dbname = d8ns8m1tf6v9l9";
+	$username = "user = iownshvjtdjjko";
+	$password = "password = 8d27ac8058ed321e3ddb34670ec04480780394f06e8e42dd2161663ee5616b18";
 
 	$maxLoginAttempt = 6;
 
