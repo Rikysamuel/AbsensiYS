1c1d59d3d939d4a64f2b91ef7b3d39ad8f40b837
Rikysamuel <rikysamuel@xtremax.com>
2017-10-24 17:42:19 +0700

    add semicolon

--- Manager/ConstantLinker.WEB.php
+++ Manager/ConstantLinker.WEB.php
@@ -1,7 +1,7 @@
 <?php
 	include $_SERVER["DOCUMENT_ROOT"].'/Manager/Constants.php';
 
-	$url = parse_url(getenv('DATABASE_URL'))
+	$url = parse_url(getenv('DATABASE_URL'));
 
 	$server = "host = ".$url["host"]." port = ".$url["port"];
 	$database = "dbname = ".$url;
