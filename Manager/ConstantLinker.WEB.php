4d27b84b7cb3bed1be7aaec8713437ef55ea9748
Rikysamuel <rikysamuel@xtremax.com>
2017-10-24 17:46:19 +0700

    fix dbname

--- Manager/ConstantLinker.WEB.php
+++ Manager/ConstantLinker.WEB.php
@@ -4,6 +4,6 @@
 	$url = parse_url(getenv('DATABASE_URL'));
 
 	$server = "host = ".$url["host"]." port = ".$url["port"];
-	$database = "dbname = ".$url;
-	$username = "user = ".$["user"];
-	$password = "password = ".$["pass"];
+	$database = "dbname = ".substr($url["path"], 1);
+	$username = "user = ".$url["user"];
+	$password = "password = ".$url["pass"];
